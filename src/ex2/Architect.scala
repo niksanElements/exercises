e87c203aefdf60a859dc86bb258e81a011215337
Nikolay Nikolov <niksan9411@gmail.com>
2017-04-17 16:22:49 +0300

    second exatrasises - part one

--- src/ex2/Architect.scala
+++ src/ex2/Architect.scala
@@ -1,4 +1,3 @@
-package ex2
 
 sealed trait Shape
 case class Triangle(a: Int, b: Int, c: Int, h: Int) extends Shape // h represent the height against the longest side of the triangle
@@ -16,10 +15,29 @@ class Architect {
    *     val o: Option[Int] = Some(6)
    *     val n: Int = o.get
    */
-  def max(xs: List[Int]): Option[Int] = ???
+  def max(xs: List[Int]): Option[Int] = {
+    def loop(in:List[Int],currentMax: Int): Int = {
+      if(in.size != 0) {
+        if(currentMax < in.head)
+          return loop(in.tail,in.head)
+        else
+          return loop(in.tail,currentMax)
+      }
+      return currentMax;
+    }
+
+    val maxEl = loop(xs,0)
+    val result = Option[Int](maxEl)
+    result
+  }
 
   // Determines the type of given triangle: "rectangular", "equilateral", "isosceles", "random"
-  def triangleType(t: Triangle): String = ???
+  def triangleType(t: Triangle): String = {
+    if(t.a == t.b == t.c) return "equilateral"
+    if(t.a == t.b) return"isosceles"
+    if((t.a*t.a+t.b*t.b) == (t.c*t.c)) return "rectangular"
+    return "random"
+  }
 
   /*
    * Calculates the area of the provided shape, by using these formulae:
@@ -31,7 +49,18 @@ class Architect {
    *  
    *  Hint: for triangles use the max function
    */
-  def area(s: Shape): Double = ???
+  def area(s: Shape): Double = s match{
+    case x: Trianlge =>{
+      if(triangleType(x) == "rectangular") return x.a* x.b/2.0
+      else{
+        val largestSide = max(List[Int](x.a,x.b,x.c))
+        return largestSide * x.h/2.0
+      }
+    }
+    case x: Rectangle => x.a * x.b
+    case x: Trapezoid => (x.a + x.b)*x.h/2.0
+    case x: Cube => -1
+  }
 
   /*
    *  Returns the number of rectangular triangles in given list of shapes
@@ -43,3 +72,17 @@ class Architect {
     iter(???, ???)
   }
 }
+
+object test{
+  def main (args: Array[String]){
+    val architect = new Architect();
+
+    val list = List[Int](1,2,3,4,6,2,4,1,6,8,3)
+    println(architect.max(list).get)
+
+    val triangle = Triangle(15,2,3,4)
+
+    println(architect.area(triangle))
+  }
+}
+
